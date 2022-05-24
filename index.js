67ee582845d992477a32c80404e864c40973a56d
roysaucedo <roy.a.saucedo@gmail.com>
2022-05-24 18:08:15 +0200

    js work

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// VARAIABLES
+
+// const burgersToggleBtn = document.querySelector('#burgersBtn');
+// const sidesToggleBtn = document.querySelector('#sidesBtn');
+// const drinkToggleBtn = document.querySelector('#drinksBtn');
+
+// const burgersMenuItems = document.querySelector('#burgersList');
+// const sidesMenuItems = document.querySelector('#sidesList');
+// const drinkssMenuItems = document.querySelector('#drinksList');
+
+
+// BURGER BTN FUNCTION
+// burgersToggleBtn.addEventListener('click',() => {
+//     if(burgersMenuItems.style.display === 'block') {
+//         burgersMenuItems.style.display = 'none';
+//     } else {
+//         burgersMenuItems.style.display = 'block';
+//     }
+// });
+
+function burgerBtnClick() {
+    if(document.querySelector('#burgersList').style.display = 'none') {
+        document.querySelector('#burgersList').style.display = 'block';
+    } 
+    else {
+        document.querySelector('#burgersList').style.display = 'block';
+    }
+    
+}
