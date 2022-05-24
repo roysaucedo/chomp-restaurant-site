32cf3bcd7a5cfdecfdb5310e472b4ea4f3a7abdb
roysaucedo <roy.a.saucedo@gmail.com>
2022-05-24 18:35:57 +0200

    Toggle Btn

--- index.js
+++ index.js
@@ -22,9 +22,53 @@
 function burgerBtnClick() {
     if(document.querySelector('#burgersList').style.display = 'none') {
         document.querySelector('#burgersList').style.display = 'block';
+        document.querySelector('#sidesList').style.display = 'none';
+        document.querySelector('#drinksList').style.display = 'none';
+        document.querySelector('#burgersBtn').style.background = '#5FA5F9';
+        document.querySelector('#burgersBtn').style.color = '#FFFFFF';
+        document.querySelector('#sidesBtn').style.background = '#EFF6FF';
+        document.querySelector('#sidesBtn').style.color = '#5FA5F9';
     } 
     else {
         document.querySelector('#burgersList').style.display = 'block';
+    }
+    
+}
+
+
+function sidesBtnClick() {
+    if(document.querySelector('#sidesList').style.display = 'none') {
+        document.querySelector('#sidesList').style.display = 'block';
+        document.querySelector('#burgersList').style.display = 'none';
+        document.querySelector('#drinksList').style.display = 'none';
+        document.querySelector('#sidesBtn').style.background = '#5FA5F9';
+        document.querySelector('#sidesBtn').style.color = '#FFFFFF';
+        document.querySelector('#burgersBtn').style.background = '#EFF6FF';
+        document.querySelector('#burgersBtn').style.color = '#5FA5F9';
+        document.querySelector('#drinksBtn').style.background = '#EFF6FF';
+        document.querySelector('#drinksBtn').style.color = '#5FA5F9';
+    } 
+    else {
+        document.querySelector('#sidesList').style.display = 'block';
+    }
+    
+}
+
+
+function drinksBtnClick() {
+    if(document.querySelector('#drinksList').style.display = 'none') {
+        document.querySelector('#drinksList').style.display = 'block';
+        document.querySelector('#burgersList').style.display = 'none';
+        document.querySelector('#sidesList').style.display = 'none';
+        document.querySelector('#drinksBtn').style.background = '#5FA5F9';
+        document.querySelector('#drinksBtn').style.color = '#FFFFFF';
+        document.querySelector('#burgersBtn').style.background = '#EFF6FF';
+        document.querySelector('#burgersBtn').style.color = '#5FA5F9';
+        document.querySelector('#sidesBtn').style.background = '#EFF6FF';
+        document.querySelector('#sidesBtn').style.color = '#5FA5F9';
+    } 
+    else {
+        document.querySelector('#drinksList').style.display = 'block';
     }
     
 }
